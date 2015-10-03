f71a28d0bdafcbbdeb129b354420782b92d39390
Jaswitha <jvankine@asu.edu>
2015-10-03 13:50:59 -0700

    UserDao interface

--- src/main/java/com/asu/cse545/group12/dao/UserDao.java
+++ src/main/java/com/asu/cse545/group12/dao/UserDao.java
@@ -1,1 +1,17 @@
+package com.asu.cse545.group12.dao;
 
+import java.util.List;
+
+import com.asu.cse545.group12.domain.Users;
+
+public interface UserDao {
+	public int insertRow(Users user);
+
+	//public List<Users> getList();
+
+	public Users getRowById(int id);
+
+	public int updateRow(Users user);
+
+	//public int deleteRow(int id);
+}
