0b4b660474d35333739ebb093e8495666d1af753
Nagarjuna Myla <chasethenag420@gmail.com>
2015-10-09 21:29:23 -0700

    Adding redirection to home page based on role

---
+++ src/main/java/com/asu/cse545/group12/authentication/SbsAuthenticationHandler.java
@@ -0,0 +1,134 @@
+package com.asu.cse545.group12.authentication;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+
+public class SbsAuthenticationHandler implements AuthenticationSuccessHandler {
+
+/*	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+		String prefix="/../WEB-INF/jsp/"; 
+		String individualTargetUrl = prefix+"individual.jsp";
+		String merchantTargetUrl = prefix+"merchant.jsp";
+		String regularTargetUrl = prefix+"regular.jsp";
+		String managerTargetUrl = prefix+"manager.jsp";
+		String adminTargetUrl = prefix+"admin.jsp";
+		String defaultTargetUrl = prefix+"home.jsp";
+		Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+		if (roles.contains("ROLE_ADMIN")) {
+			getRedirectStrategy().sendRedirect(request, response, adminTargetUrl);
+		} else if (roles.contains("ROLE_MANAGER")) {
+			getRedirectStrategy().sendRedirect(request, response, managerTargetUrl);
+		} else if (roles.contains("ROLE_REGULAR")) {
+			getRedirectStrategy().sendRedirect(request, response, regularTargetUrl);
+		}else if (roles.contains("ROLE_MERCHANT")) {
+			getRedirectStrategy().sendRedirect(request, response, merchantTargetUrl);
+		}else if (roles.contains("ROLE_INDIVIDUAL")) {
+			getRedirectStrategy().sendRedirect(request, response, individualTargetUrl);
+		}else {
+			getRedirectStrategy().sendRedirect(request, response, defaultTargetUrl);
+		}
+	}*/
+	private static final Logger logger = Logger.getLogger(SbsAuthenticationHandler.class);
+	 
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+ 
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, 
+      HttpServletResponse response, Authentication authentication) throws IOException {
+        handle(request, response, authentication);
+        clearAuthenticationAttributes(request);
+    }
+ 
+    protected void handle(HttpServletRequest request, 
+      HttpServletResponse response, Authentication authentication) throws IOException {
+        String targetUrl = determineTargetUrl(authentication);
+ 
+        if (response.isCommitted()) {
+            logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
+            return;
+        }
+ 
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
+ 
+    /** Builds the target URL according to the logic defined in the main class Javadoc. */
+    protected String determineTargetUrl(Authentication authentication) {
+    	boolean isIndividual = false;
+    	boolean isMerchant = false;
+    	boolean isRegular = false;
+    	boolean isManager = false;
+        boolean isAdmin = false;
+        
+        String prefix="/"; 
+        String suffix=".html";
+		String individualTargetUrl = prefix+"individual"+suffix;
+		String merchantTargetUrl = prefix+"merchant"+suffix;
+		String regularTargetUrl = prefix+"regular"+suffix;
+		String managerTargetUrl = prefix+"manager"+suffix;
+		String adminTargetUrl = prefix+"admin"+suffix;
+		String defaultTargetUrl = prefix+"home"+suffix;
+
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for (GrantedAuthority grantedAuthority : authorities) {
+            if (grantedAuthority.getAuthority().equals("ROLE_MANAGER")) {
+            	isManager = true;
+                break;
+            } else if (grantedAuthority.getAuthority().equals("ROLE_ADMIN")) {
+                isAdmin = true;
+                break;
+            }else if (grantedAuthority.getAuthority().equals("ROLE_REGULAR")) {
+            	isRegular = true;
+                break;
+            }else if (grantedAuthority.getAuthority().equals("ROLE_MERCHANT")) {
+            	isMerchant = true;
+                break;
+            }else if (grantedAuthority.getAuthority().equals("ROLE_INDIVIDUAL")) {
+            	isIndividual = true;
+                break;
+            }
+        }
+ 
+        if (isIndividual) {
+            return individualTargetUrl;
+        } else if (isMerchant) {
+            return merchantTargetUrl;
+        }else if (isRegular) {
+            return regularTargetUrl;
+        }else if (isManager) {
+            return managerTargetUrl;
+        }else if (isAdmin) {
+            return adminTargetUrl;
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+ 
+    protected void clearAuthenticationAttributes(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return;
+        }
+        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+    }
+ 
+    public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+        this.redirectStrategy = redirectStrategy;
+    }
+    protected RedirectStrategy getRedirectStrategy() {
+        return redirectStrategy;
+    }
+}
--- src/main/webapp/WEB-INF/jsp/home.jsp
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -2,6 +2,6 @@
 <head>
 </head>
 <body>    
-    <h1>I am a home jsp</h1>
+    <h1>Doesn't seem to you have a proper role or your account is disabled please contact admin</h1>
 </body>
 </html>
